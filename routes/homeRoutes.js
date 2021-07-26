e6d43172df3a12f5262f7ae4a6c354b8b26375b7
Henri <ecch9210@gmail.com>
2021-07-26 18:02:44 -0500

    Fixed homeRoutes file

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,15 +2,16 @@ const path = require("path");
 const router = require('express').Router();
 
 
-router.get("/", (res) => {
+//Render homepage
+router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-
-router.get("/exercise", (res) => {
+//Render excercise page
+router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-
-  router.get("/stats", (res) => {
+//Render stats page
+  router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
